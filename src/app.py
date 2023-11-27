9f2432808d078e2003093247ed1c87a269b2d804
Giuseppe <132507608+GiuseppeTaylorr@users.noreply.github.com>
2023-11-27 01:00:18 +0000

    liisto

--- src/app.py
+++ src/app.py
@@ -1,15 +1,12 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
 from flask_migrate import Migrate
-from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, People, Planet, User, Favorite  # Importa los modelos desde models.py
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -21,31 +18,125 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-MIGRATE = Migrate(app, db)
-db.init_app(app)
+
+api = Api(app)
+
+
+migrate = Migrate(app, db)
+
+
 CORS(app)
+
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+# Inicializa la instancia de db
+db.init_app(app)
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# perosnas
+class PeopleResource(Resource):
+    def get(self):
+        people_list = People.query.all()
+        people_data = [{"id": person.id, "name": person.name} for person in people_list]
+        return jsonify({"people": people_data})
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+class SinglePeopleResource(Resource):
+    def get(self, people_id):
+        person = People.query.get_or_404(people_id)
+        return jsonify({"id": person.id, "name": person.name, "birth_year": person.birth_year, "gender": person.gender})
 
-    return jsonify(response_body), 200
+# las rutas que hice para planets
+class PlanetsResource(Resource):
+    def get(self):
+        planets_list = Planet.query.all()
+        planets_data = [{"id": planet.id, "name": planet.name} for planet in planets_list]
+        return jsonify({"planets": planets_data})
 
-# this only runs if `$ python src/app.py` is executed
+class SinglePlanetResource(Resource):
+    def get(self, planet_id):
+        planet = Planet.query.get_or_404(planet_id)
+        return jsonify({"id": planet.id, "name": planet.name, "climate": planet.climate, "population": planet.population})
+
+
+class UsersResource(Resource):
+    def get(self):
+        users_list = User.query.all()
+        users_data = [{"id": user.id, "username": user.username, "email": user.email} for user in users_list]
+        return jsonify({"users": users_data})
+
+class SingleUserResource(Resource):
+    def get(self, user_id):
+        user = User.query.get_or_404(user_id)
+        user_data = {"id": user.id, "username": user.username, "email": user.email}
+        return jsonify(user_data)
+
+class FavoritesResource(Resource):
+    def get(self, user_id):
+        favorites_list = Favorite.query.filter_by(user_id=user_id).all()
+        favorites_data = [{"id": fav.id, "user_id": fav.user_id, "planet_id": fav.planet_id, "people_id": fav.people_id} for fav in favorites_list]
+        return jsonify({"favorites": favorites_data})
+
+class AddFavoriteResource(Resource):
+    def post(self, user_id, planet_id=None, people_id=None):
+        if planet_id is not None:
+            # Lógica para añadir un planeta a los favoritos del usuario actual
+            favorite = Favorite(user_id=user_id, planet_id=planet_id)
+            db.session.add(favorite)
+            db.session.commit()
+            return jsonify({"message": f"Planet {planet_id} added to favorites for User {user_id}"})
+        elif people_id is not None:
+            # Lógica para añadir una persona a los favoritos del usuario actual
+            favorite = Favorite(user_id=user_id, people_id=people_id)
+            db.session.add(favorite)
+            db.session.commit()
+            return jsonify({"message": f"People {people_id} added to favorites for User {user_id}"})
+        else:
+            return jsonify({"error": "Invalid request"})
+
+class DeleteFavoriteResource(Resource):
+    def delete(self, user_id, planet_id=None, people_id=None):
+        if planet_id is not None:
+            # Lógica para eliminar un planeta de los favoritos del usuario actual
+            favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+            if favorite:
+                db.session.delete(favorite)
+                db.session.commit()
+                return jsonify({"message": f"Planet {planet_id} removed from favorites for User {user_id}"})
+            else:
+                return jsonify({"error": "Favorite not found"})
+        elif people_id is not None:
+            # Lógica para eliminar una persona de los favoritos del usuario actual
+            favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()
+            if favorite:
+                db.session.delete(favorite)
+                db.session.commit()
+                return jsonify({"message": f"People {people_id} removed from favorites for User {user_id}"})
+            else:
+                return jsonify({"error": "Favorite not found"})
+        else:
+            return jsonify({"error": "Invalid request"})
+
+
+api.add_resource(PeopleResource, '/people')
+api.add_resource(SinglePeopleResource, '/people/<int:people_id>')
+api.add_resource(PlanetsResource, '/planets')
+api.add_resource(SinglePlanetResource, '/planets/<int:planet_id>')
+api.add_resource(UsersResource, '/users')
+api.add_resource(SingleUserResource, '/users/<int:user_id>')
+api.add_resource(FavoritesResource, '/users/<int:user_id>/favorites')
+api.add_resource(AddFavoriteResource, '/favorites/user/<int:user_id>/planet/<int:planet_id>', '/favorites/user/<int:user_id>/people/<int:people_id>')
+api.add_resource(DeleteFavoriteResource, '/favorites/user/<int:user_id>/planet/<int:planet_id>', '/favorites/user/<int:user_id>/people/<int:people_id>')
+
+# ...
+
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    db.create_all()
+    app.run(host='0.0.0.0', port=3000, debug=True)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,31 @@
+# models.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_year = db.Column(db.String(20))
+    gender = db.Column(db.String(20))
+    favorites = db.relationship('Favorite', backref='people', lazy=True)
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    climate = db.Column(db.String(50))
+    population = db.Column(db.String(20))
+    favorites = db.relationship('Favorite', backref='planet', lazy=True)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
